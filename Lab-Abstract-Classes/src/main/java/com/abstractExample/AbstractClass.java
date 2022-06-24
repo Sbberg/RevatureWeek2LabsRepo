e998663d11940a60f91093fd31a271b5614d0a31
Shane <Sbbergeron98@gmail.com>
2022-06-24 13:08:26 -0700

    initial commit Abstract lab

---
+++ Lab-Abstract-Classes/src/main/java/com/abstractExample/AbstractClass.java
@@ -0,0 +1,16 @@
+package com.abstractExample;
+
+public class AbstractClass {
+
+    public void AbstractClass(){
+
+    }
+    protected String name;
+
+    public abstract String getName();
+    public abstract void setName(String name);
+
+
+
+
+}
